111574c4488fa1f77899827fd6b7008f39646eff
Patrik Malta <maltapatrik3@gmail.com>
2024-02-29 09:07:27 -0400

    version 1.0

--- climalta/src/api.jsx
+++ climalta/src/api.jsx
@@ -10,6 +10,7 @@ function ApiJs(){
     const [umidade, SetUmid] = useState(null)
     const [Rweather, SetWeather] = useState(null)
     const [RealWeather, SetRealWeather] = useState(null)
+    const [Swind, SetWind] = useState(null)
     const [count, setCount] = useState(0)
     const urlIcon = `https://openweathermap.org/img/wn/${Rweather}@2x.png`
 
@@ -38,6 +39,7 @@ function ApiJs(){
                 SetTempMin(data.main.temp_min)
                 SetWeather(data.weather[0].icon)
                 SetRealWeather(data.weather[0].description)
+                SetWind(data.wind.speed)
             }).catch(error => {
                 console.log('deu erro: ', error)
             })
@@ -48,9 +50,17 @@ function ApiJs(){
 
     return(
         <div className='main'>
+            <div>
+                <nav>
+                    <ul>
+                        <li><a href="https://www.instagram.com/usuario_progamador?igsh=ZG9tMTFncG9lbGl0">@usuario_progamador</a></li>
+                    </ul>
+                </nav>
+            </div>
+
             <div className='header'>
-                <h1>Cidade : {cidade}</h1>
-                <h1>País : {país}</h1>
+                <h1>{cidade}</h1>
+                <h1>{país}</h1>
             </div>
             <img src={urlIcon} alt=""/>
             <h1>{RealWeather}</h1>
@@ -64,6 +74,11 @@ function ApiJs(){
                 <h1>Nebulosidade: {altNuvens}% </h1>
                 <h1>Humidade: {umidade}</h1>
             </div>
+            <h1>Vento: {Swind} km/h</h1>
+            <footer>
+                <p>&copy; 2024 - usuario_progamador</p>
+            </footer>
+
         </div>
     )
 }
